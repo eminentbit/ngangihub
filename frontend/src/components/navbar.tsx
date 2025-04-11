e9cb296432c18058c4d6e89d01533652bb1d1f2b
Damaris200 <151961679+Damaris200@users.noreply.github.com>
2025-04-11 22:46:48 +0100

    sign

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
               </Link>
             </li>
           </ul>
-           {/*<div className="flex items-center gap-4">
+           <div className="flex items-center gap-4">
             <button
               className="bg-gray-200 text-sm font-semibold text-gray-700 px-6 py-2 rounded-md hover:bg-gray-300 transition duration-300"
               onClick={() => navigate("/signup")}
@@ -46,7 +46,7 @@ export default function Navbar() {
             >
               Login
             </button> 
-          </div>*/}
+          </div>*
         </nav>
 
         {/* Hamburger icon for mobile navigation */}
