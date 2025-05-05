a6c85aab1e7c5064577ccfc4c125ec04edd0a35d
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-05-05 17:30:55 +0100

    modifying the main.tsx file to be able to use the theme provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";      
 import { router } from "./routes";
+import { ThemeProvider } from "./pages/dashboard.bod.pages/ThemeContext"; // Adjust path as needed
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
