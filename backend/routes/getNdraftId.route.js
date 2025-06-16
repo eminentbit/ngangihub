03d0c601691fe6c24ea1e252600f09731723fc43
eminentbit <ejohdaryl@gmail.com>
2025-06-16 16:21:09 +0100

    added limiters

--- backend/routes/getNdraftId.route.js
+++ backend/routes/getNdraftId.route.js
@@ -1,8 +1,11 @@
-import express from 'express';
-import getNjangiDraftId from '../controllers/getNdrafId.controller.js';
+import express from "express";
+import getNjangiDraftId from "../controllers/getNdrafId.controller.js";
+import limiter from "../middleware/limiter.js";
 
 const router = express.Router();
 
-router.get("/get-ndraft-id", getNjangiDraftId)
+router.use("/", limiter);
+
+router.get("/get-ndraft-id", getNjangiDraftId);
 
 export default router;
