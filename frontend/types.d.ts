0eda9a46fd8e8bcee966d126127cf1f9af2190a9
eminentbit <ejohdaryl@gmail.com>
2025-06-25 11:18:02 +0100

    backend only

--- frontend/types.d.ts
+++
@@ -1,7 +0,0 @@
-import "react-hook-form";
-
-declare module "react-hook-form" {
-  interface RegisterOptions {
-    type?: "custom" | "manual" | "value" | "onBlur" | "onChange" | "required" | "min" | "max" | "maxLength" | "minLength" | "validate" | "pattern" | "valueAsNumber" | "valueAsDate";
-  }
-}
